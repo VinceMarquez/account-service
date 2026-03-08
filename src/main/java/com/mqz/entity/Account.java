3ea1b76ec96dd6ddf2f6474b56bc7b73c3f1348a
Vince <vince.marquez10@gmail.com>
2026-03-08 10:39:27 +0800

    Remove redundant annotation

--- src/main/java/com/mqz/entity/Account.java
+++ src/main/java/com/mqz/entity/Account.java
@@ -1,7 +1,6 @@
 package com.mqz.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "accounts")
+@Entity(name = "accounts")
 @Data
 @Builder
 @AllArgsConstructor
